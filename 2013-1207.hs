e8dea1493faef66978a930f334c185d87b68def3
evx001 <eviexeris@yahoo.com>
2013-12-07 22:46:42 -0500

    sumRev

--- 2013-1207.hs
+++ 2013-1207.hs
@@ -5,12 +5,34 @@ swap (a,b) = (b,a)
 -- let 
 perennials = [("C",[1,2,1,1]),("Java",[2,1,4,0]),("Objective_C",[3,38,0,0]),("C++",[4,3,2,6]),("C#",[5,7,0,0]),("PHP",[6,5,0,0]),("Visual_Basic",[7,4,5,5]),("Python",[8,6,29,0]),("Transact_SQL",[9,45,0,0]),("JavaScript",[10,10,14,0]),("Lisp",[15,21,17,3]),("COBOL",[19,17,3,12]),("Ada",[22,23,18,2])]
 
-revPerennials = [swap (perennials!!n) | n <- [0..12]]
+[sum(fst(swap (perennials!!n))) | n <- [0..12]]
 
-revRevPerenn = reverse revPerennials
 
 {-
-------------------------------------------------------------------------
+------------------------------------------------------------------------ 
+let sumRevPeren =  zip   [sum(fst(swap (perennials!!n))) | n <- [0..12]]   [snd(swap (perennials!!n)) | n <- [0..12]] 
+
+
+
+
+*Dec7>  zip   [sum(fst(swap (perennials!!n))) | n <- [0..12]]   [snd(swap (perennials!!n)) | n <- [0..12]] 
+[(5,"C"),(7,"Java"),(41,"Objective_C"),(15,"C++"),(12,"C#"),(11,"PHP"),(21,"Visual_Basic"),(43,"Python"),(54,"Transact_SQL"),(34,"JavaScript"),(56,"Lisp"),(51,"COBOL"),(65,"Ada")]
+
+
+*Dec7> [sum(fst(swap (perennials!!n))) | n <- [0..12]]
+[5,7,41,15,12,11,21,43,54,34,56,51,65]
+
+*Dec7> [snd(swap (perennials!!n)) | n <- [0..12]]
+["C","Java","Objective_C","C++","C#","PHP","Visual_Basic","Python","Transact_SQL","JavaScript","Lisp","COBOL","Ada"]
+
+*Dec7> zip  [snd(swap (perennials!!n)) | n <- [0..12]] [sum(fst(swap (perennials!!n))) | n <- [0..12]]
+[("C",5),("Java",7),("Objective_C",41),("C++",15),("C#",12),("PHP",11),("Visual_Basic",21),("Python",43),("Transact_SQL",54),("JavaScript",34),("Lisp",56),("COBOL",51),("Ada",65)]
+
+
+revPerennials = [swap (perennials!!n) | n <- [0..12]]
+revRevPerenn = reverse revPerennials
+
+
 minimum (tail revPerennials) 
 *Dec7> if minimum revPerennials == revPerennials!!0 then revPerennials!!0 else revPerennials!!1
 ([1,2,1,1],"C")
