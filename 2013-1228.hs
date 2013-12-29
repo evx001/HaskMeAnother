dd4e336cf1cf0d46cd9f8d4781b1b2b71a8afe28
evx001 <eviexeris@yahoo.com>
2013-12-28 21:21:17 -0500

    Haskell: for when your test base exceeds your code base.

--- 2013-1228.hs
+++ 2013-1228.hs
@@ -1,1 +1,20 @@
-module Dec28 where 
+module Dec28 where
+-- haskellforcats@gmail.com
+
+-- Haskell: because proofs are proof!
+-- Haskell: for when your test-base exceeds your code-base.
+
+{- 
+99.9% have failed to solve this.
+
+if 
+3 = 18 
+4 = 32
+5 = 50 
+6 = 72
+7 = 98
+
+then 
+
+10 = x 
+-}
