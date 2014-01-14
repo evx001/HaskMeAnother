3018e46d9bae643b6d3e0484e012da50d1808ddb
evx001 <eviexeris@gmail.com>
2014-01-13 23:52:53 -0500

    Trees in Context

---
+++ 2014-0112-Trees.hs
@@ -0,0 +1,58 @@
+Applicants, with Clojure 8x more likely to to get the nod at sixSig. 
+
+Dendrologist 
+
+    trees 
+        the world out side is a mess 
+    
+    Thoughts about trees 
+        pointers go down 
+            parents to children 
+        pointers up 
+            database 
+        linked list 
+        Diamond 
+            acyclic 
+            conical 
+            a graph
+        
+        cycle isn't a tree because we don't
+        
+        pointer cycles are the tail of all evil as well as the root
+
+        B[D] -> B[E] 
+        
+        cycle's have to be copied in their entirety 
+        generally trees don't
+            unless there are dopey up-pointers
+        add in a level of indirection 
+        
+        "Context is King" 
+        
+        symbols: the Original Identities 
+        Context     
+        Not just for identities 
+            
+        Context in action. 
+            where are you missing it? 
+        
+`       use extensible maps
+            like a Json obj sort of 
+            not a straight up list ['a'['b'[c]'d']'e']
+                cause you will have to revisit it to add in identifiers 
+            context in your stack 
+                reify that state 
+            State is mostly an illusion when we are in clojure. 
+               zipper function 
+          
+        strict eval is procedural ultimately 
+            Lazy is only somewhat less procedural
+        
+        Breadth first numbering 
+        Chris Okasaki 
+        Backtracking iterators 
+        Jean Christopher Filliatre
+        
+        Consider
+        Identities and contexts 
+             
