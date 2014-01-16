d96b35cce96699e47bf35339436e53b1a730c981
evx001 <eviexeris@gmail.com>
2014-01-15 22:42:17 -0500

    cut Data.Char stuff

--- 2014-0115.hs
+++ 2014-0115.hs
@@ -4,6 +4,8 @@ module Jan15 where
 import qualified Data.List as D -- for genericLength etc. to be used as D.genericLength
 -- import GHC.Base
 -- import Data.Char 
+
+{- thirty first day of continuous Haskell -} 
 -------------------------------------------------------------
 pYths n           = [(x,y,z)|   x <- [1..n],
                                 y <- [1..n],
@@ -15,18 +17,6 @@ nestedConcat t1 t2 =  concat [[(x,y) | y <- [t1]]|x <- [t2]]
 -- > nestedConcat [1..11] [1..7]
 -- [([1,2,3,4,5,6,7],[1,2,3,4,5,6,7,8,9,10,11])]
 
-{---------------------------------------------------------------- 
-sHift                       :: Int -> Char -> Char 
-sHift   n c | isLower c     = int2low ((low2int c + n) `mod`26)
-            | isUpper c     = int2upp ((upp2int c + n) `mod`26)
-            | otherwise     = c  
-
-fReqs                       :: String -> [Float] 
-fReqs   xs                  = [percent (count x xs') n | x <- ['a'..'z']]
-                                where 
-                                    xs' = map toLower xs 
-                                    n   = letters xs
-----------------------------------------------------------------}
 ins2rt 				:: Ord a => a->[a]->[a]
 ins2rt x []			= [x] 
 ins2rt x (y:ys) |x<=y		= x:y:ys
