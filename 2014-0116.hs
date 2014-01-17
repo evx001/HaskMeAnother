29e4abe7edde80bb502cbe426baaffcdb7723e86
evx001 <eviexeris@gmail.com>
2014-01-16 21:54:27 -0500

    images

---
+++ 2014-0116.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE NPlusKPatterns #-}
+module Jan16 where
+-- ghci -XNPlusKPatterns
+import qualified Data.List as D -- for genericLength etc. to be used as D.genericLength
+-- import GHC.Base
+-- import Data.Char 
+
+{- thirty first day of continuous Haskell -} 
+
+
