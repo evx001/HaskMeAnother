098a5c89c48abaa29014468eba5d19ae5723d476
evx001 <eviexeris@yahoo.com>
2014-01-19 00:06:22 -0500

    ParameterizedTypes with illustration

--- 2014-0118-parametriszedTypes.hs
+++ 2014-0118-parametriszedTypes.hs
@@ -1,10 +1,28 @@
 module Jan18 where 
 -- :   set expandtab ts=4 ruler number spell 
--- ----------------------------------------------------------
+import qualified Data.Maybe as M
+----------------------------------------------------------
 --          Parameterized Datatypes 
 -- ---------------------------------------------------------
--- the maybe is the potentual to have a type if "a" type is returned.  (Maybe a) is not a type in itself but a facilitator of types. 
+-- the maybe is the potential to have a type if "a" type is returned. So types that include and "a"  (Maybe a) are said to be parameterized because they are incomplete with out the parameter.  If it gets it's type var then it can go on to become a complete type. 
+{- 
 data Maybe a = Just a | Nothing 
 
+x           :: Maybe Int 
+x           = Nothing 
 
+fromMaybe   :: a -> Maybe a -> a 
 
+fromMaybe defaultVal Nothing    = defaultVal 
+
+fromMaybe _ (Just x)            = x
+-}
+-- for explication see
+-- figure ParameterizedTypes01a.jpeg 
+
+-- a standard list data type would resemble this.
+-- data List a = Empty | Cons a (List a) 
+
+-- data Map k a = ...
+
+-- Parameterized Types can hold values of any type.   
