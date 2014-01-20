132c87e151386d0eab92206798f8fca8aee1b8a9
evx001 <eviexeris@yahoo.com>
2014-01-19 22:15:54 -0500

    Reading File ---> Console

--- 2014-0119-IO.hs
+++ 2014-0119-IO.hs
@@ -1,13 +1,13 @@
-module Jan19 where 
+module Main where 
 -- :   set expandtab ts=4 ruler number spell 
 
 -- Prelude> :i putStrLn 
 -- putStrLn :: String -> IO () 	-- Defined in `System.IO'
-main = putStrLn "Hello From Haskell" 
+-- main = putStrLn "Hello From Haskell" 
 -- *Jan19> :i main 
 -- main :: IO () 	-- Defined at 2014-0119-IO.hs:6:1
 -- NOTE () stands for unit, Like void in C#. It's a place holder. 
-data Unit = Unit    
+-- data Unit = Unit    
 {- IO is a parameterized type, with a type variable any type.
 newtype IO a
   = GHC.Types.IO (GHC.Prim.State# GHC.Prim.RealWorld
@@ -20,4 +20,6 @@ instance Functor IO -- Defined in `GHC.Base'
 -- IO ()
 -- returns a Unit, an IO Action similar to a Maybe in that it could do something outside. 
 -- the IO Action would have to be used in main to get anything to happen.  
-
+main :: IO () 
+main = do
+    putStrLn "Say something "  
--- 2014-0119.hs
+++ 2014-0119.hs
@@ -1,8 +1,6 @@
 module Jan19 where 
 -- : set expandtab ts=4 ruler number spell
 
-import System.Environment
-import System.Directory
-import System.IO
-import Control.Exception
-import qualified Data.ByteString.Lazy as B
+-- import qualified Data.ByteString.Lazy as B 
+
+
