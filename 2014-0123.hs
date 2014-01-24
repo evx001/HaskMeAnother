3b6b99f5dc4fb971ca50ca86f570c21c89c61ba5
evx001 <eviexeris@yahoo.com>
2014-01-24 18:49:46 -0500

    EMACS

---
+++ 2014-0123.hs
@@ -0,0 +1,18 @@
+module Jan23 where 
+
+-- : set expandtab ts=4 ruler number spell
+
+{- 
+;; The melpa repo
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+-}
+
