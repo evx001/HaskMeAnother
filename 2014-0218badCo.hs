103cddf82d35118d1ede4347863e977eb5dcdfc4
evx001 <eviexeris@yahoo.com>
2014-02-18 19:35:27 -0500

    badCo -> wpSec?

--- 2014-0218badCo.hs
+++ 2014-0218badCo.hs
@@ -1,71 +1,12 @@
-module Badco where 
-data Badco 
-[
-9. Fiserv
-> Rating: 2.5
-> Number of reviews: 440
-> CEO approval rating: 40% (Jeffery Yabuki)
-> Employees: 20,000
-]
-[
-8. NCR
-> Rating: 2.5
-> Number of reviews: 385
-> CEO approval rating: 39% (Bill Nuti)
-> Employees: 25,700
-]
-[
-7. Sears Holdings (Sears/ KMart)
-> Rating: 2.5
-> Number of reviews: 583
-> CEO approval rating: 19% (Eddie Lampert)
-> Employees: 274,000
-]
-[
-6. ADT
+module WpSec where 
+{- 
+wordpress.org 
+	job board 
+ 
+Tim 
+	Zemanta Mobile 2007 -- 
+		understanding content 
 
-> Rating: 2.4
-> Number of reviews: 309
-> CEO approval rating: 55% (Naren Gursahaney)
-> Employees: 16,000
-]
-[
-5. RadioShack
+	publishers and bloggers
+	 
 
-> Rating: 2.4
-> Number of reviews: 868
-> CEO approval rating: 38% (Joseph Magnacca)
-> Employees: 34,500
-]
-[
-4. Dollar General
-
-> Rating: 2.4
-> Number of reviews: 375
-> CEO approval rating: 43% (Rick Dreiling)
-> Employees: 90,500
-]
-[
-3. Dillard’s
-
-> Rating: 2.3
-> Number of reviews: 560
-> CEO approval rating: 23% (Bill Dillard II)
-> Employees: 27,740
-]
-[
-2. Express Scripts
-
-> Rating: 2.3
-> Number of reviews: 312
-> CEO approval rating: 36% (George Paz)
-> Employees: 30,215
-]
-[
-1. DISH
-
-> Rating: 2.3
-> Number of reviews: 831
-> CEO approval rating: 40% (Joseph Clayton)
-> Employees: 35,000
-]
