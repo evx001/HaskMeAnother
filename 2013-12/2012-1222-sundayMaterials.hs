59ca4c33152c9007547d6479e95403c53e96001e
evx001 <eviexeris@yahoo.com>
2014-02-23 12:02:52 -0500

    sundayMatUpdate

--- 2013-12/2012-1222-sundayMaterials.hs
+++ 2013-12/2012-1222-sundayMaterials.hs
@@ -1,10 +1,14 @@
 {-
 Materials for Sundays 
-	white extension chord
-	Big Backpack	
+	2 - white extension chords
+	"3-2" adapters 
+	"1-3" expanders 
+	crateHandTruck
+	X-stand 
+	LapDesk
 	Mac 	+ powerChord
 	Asus 	+ powerChord 
 *** jambox 	+ powerchord ??
 	Big NoteBook for Signage
-	Text Book
+	Hutton Book
 -}	
