b874f1ebb89449658b9ef07e3b7c116223546586
evx001 <eviexeris@yahoo.com>
2014-02-25 15:39:03 -0500

    CloudFoundry

---
+++ 2014-0225cloudFoundry.hs
@@ -0,0 +1,85 @@
+{- CloudFoundry 
+Paas
+eventMachine & Sinatra
+	container not a complete OS 
+		Build pack make it open source so community will make it so we support many lang
+		Build packs run in order to best first match
+		droplet 
+			Heroku Slug like 
+				script and dependancies 
+		Staging gets more mem and is distroyed after to free up mem for app
+			compiled app 
+				can be dropped into multiple spaces. 
+
+	benifit starts quicker
+		But theres a resource cap
+	Using Ruby
+		for management 	
+	Executers that run the apps broadcast across network
+		Alogolrthm 
+		Random as go load balancing tech
+		routing table 
+
+? can you give us a figurative bench mark for for what a container just for comparison sake
+	
+? what are the pain points
+
+Your Gui is your Gui
+
+ 
+Services Market-Place 
+	companies that provide services 
+
+others are porpriety  
+			Chef 
+			Aws Cloud Formation 
+			Canonical Juju
+
+Bosh is the deployment tool 
+	"It's awesome"
+	Opensource
+
+	Deploycluster pulls StemCell(image) 
+		manifest discribes the cluster 
+			st.yml
+	Bosh compiles your image from source 
+		we want you to compile to Bosh 
+
+Delcarative 
+Idempotent 
+Scalable to big Clusters 
+Cloud agnostic 
+O.S. agnostic
+	
+Future
+	Improve Stablity 
+	GOlANG REWRITE 
+	more clouds 
+	Support more OSs
+		getting windows
+			they need it bad 
+	All Pivotal Products 
+		available
+
+http://run.pivotal.io
+	give feedback
+	try to 
+	no free tier
+	but 3x cheaper than the usuals 
+	10 a month for cheapest 
+		you get email support
+	Run cloudFoundry inside your own datacenter
+	Ruby 2.1
+		build and run without internet 
+		
+	Pivotal Products 
+		
+	Enterprise Dev. 
+		the vision for them 
+			big bank 
+				has it's own in the inhouse data center 
+					and a second dev one
+	localServer 
+		and push occasionally
+		
+	
