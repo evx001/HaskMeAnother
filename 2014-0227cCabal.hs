a6923e2a5179a9f50571c8e1468e744de30dad2e
evx001 <eviexeris@yahoo.com>
2014-02-27 23:59:17 -0500

    codeCabal

--- 2014-0227cCabal.hs
+++ 2014-0227cCabal.hs
@@ -4,6 +4,8 @@ lubuntu 12.04 LTS -- build env/OS
 
 linux -- shell/bash  commands  
 
+Tcp/Ip 
+
 vim -- text edit + ubiquity (street cred) 
 
 git -- source control, version control, backup, colaboration
@@ -12,7 +14,7 @@ Haskell -- strong, fast, robust, scalable, concise programming language. 25 year
 	has a reputation for being impossibly hard, and without a mentor/guide is.  
 
 Cabal -- Haskell's pacackage system, simlar to ruby gems, linux packages, etc.
-
-codeCabal an intensive short cycle program that will that the absolute beginner both models of computation. Dev bootcamps other programs focus on the next or first job, which is useful but doesn't provide and ready them not just for the next step in programming but it's future. 
+	-- a clandestin group, 
+codeCabal an intensive short cycle program that will that the absolute beginner both models of computation. Dev bootcamps focus on first jobs which is useful but doesn't provide and ready them not just for the next step in programming but it's future. 
 
 
