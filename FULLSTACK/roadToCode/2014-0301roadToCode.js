0e2ec1b377334c892d0f214572139f41cb25237e
evx001 <eviexeris@yahoo.com>
2014-03-01 13:30:20 -0500

    Fullstack

---
+++ FULLSTACK/roadToCode/2014-0301roadToCode.js
@@ -0,0 +1,70 @@
+// roadToCode
+// WebBasics 
+// HTML/CSS
+// JS
+// JS Applied: Text Olypics Project 
+// 
+
+// practice/persistance 
+// ask if ? 
+// Details Details 
+// frustration 
+// no neg
+// Focus today
+
+/* what is the web? 
+a collection of servers 
+    many users per server
+    Linux, windows OS
+        
+    blade = individual 
+    google's ocean cooled servers
+   
+DB 
+webserver
+Backend 
+
+backend view 
+   term and ssh 
+
+elements 
+image / video 
+Front-end Code 
+
+Html 
+CSS 
+JS 
+
+Front-Viewers 
+    chrome/firefox
+    iphone safari 
+    MS-Outlook 
+
+jsBin - HTML/CSS/JS Runtime 
+
+Sublime Text 
+    org
+    color code 
+    Indentation
+
+FireFox Devtools 
+
+html -  the frame 
+css  -  the pretty 
+js   -  the waiter 
+    js goes everywhere 
+    js can touch most everything 
+
+Mozilla early web browser 
+    during the black-net days 
+    html was to help visualize things better
+    tags and text 
+    <div> Road to Code </div> 
+    mostly in pairs 
+    no "hello world" and you will always suck at that language 
+
+DRY 
+    Don't repeat yourself  
+
+
+
