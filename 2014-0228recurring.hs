9d64e2b4b7a7e542c61960b6c2a00dbdffbd197d
evx001 <eviexeris@yahoo.com>
2014-02-28 19:27:51 -0500

    recur

--- 2014-0228recurring.hs
+++ 2014-0228recurring.hs
@@ -1,2 +1,11 @@
 module Recur where 
-
+-----------------------------
+-- -----RECURSION ------------ 
+-- when a function calls itself. 
+------------------------------
+-- pow2 :: (Eq a, Num a, Num a1) => a -> a1
+pow2 n =    
+    if n == 0 -- at first this may look like we are redefining the number one.  
+    then 1    -- if the condition is zero we want a one returned. 
+    else 2 * (pow2 (n-1)) -- otherwise we want something else 
+                          --  
