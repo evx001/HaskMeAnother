d344fb01b61a27c163244d70330bf4eba3f0f470
evx001 <eviexeris@yahoo.com>
2014-03-05 01:23:42 -0500

    lists

---
+++ 2014-0305lists.hs
@@ -0,0 +1,11 @@
+module Lists where 
+
+-- Prelude>  let x = [1,2,3];
+-- Prelude> x
+-- [1,2,3]
+-- Prelude> x == (1:(2:(3:[])))
+-- True
+-- Prelude> (++) x [4,5] == (1:(2:(3:[]))) ++ (4:(5:[]))
+-- True 
+
+
