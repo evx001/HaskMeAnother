de4896da5d7aa7f3443ad6a91a1dd097d0b5a8bf
evx001 <eviexeris@gmail.com>
2014-03-06 23:39:26 -0500

    guards with module Guards001 (remove0dd,Pow2) where

---
+++ 2014-0306guards.hs
@@ -0,0 +1,24 @@
+module Guards.2014-0306guards 
+( removeOdds  
+, pow2
+) where 
+
+-- pattern matching allows us leavage the structure of data, where guards allow us to sfit data's values. 
+
+pow2 n 
+    | n == 0            = 1 
+    | otherwise         = 2 * (pow2(n-1))
+
+-- first thing to notice is "="  is way to the right of where we usually are in the functions we've seen so far. 
+-- second is the "|" which replaces the "if" "then".
+-- third is the use of otherwise, this takes the place of our "else."  
+
+
+-----------------------------------------------
+-- from the console we see. 
+-- *Guards> pow2 6
+-- 64
+removeOdds [] = [] 
+removeOdds (x:xs) 
+    | mod x 2 == 0  = x : (removeOdds xs) 
+    | otherwise     = removeOdds xs 
