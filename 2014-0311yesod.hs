1ae103a0a205d87e7a8ad1d317fa457f7d7fd544
evx001 <evxyz001@gmail.com>
2014-03-11 04:17:52 -0400

    yesod notes

---
+++ 2014-0311yesod.hs
@@ -0,0 +1,9 @@
+module Yesod where 
+--------------------------------------------------------------------
+-- haskcat1@HaskUntu2:~/HaskMeAnother$ cd yesod/
+-- haskcat1@HaskUntu2:~/HaskMeAnother/yesod$ ~/.cabal/bin/yesod init 
+-- Project name: yogsothoth 
+-- cd yogsothoth
+---------------------------------------------------------------- 
+-- cabal-dev install && yesod --dev devel 
+-----------------------------------------------------------------
