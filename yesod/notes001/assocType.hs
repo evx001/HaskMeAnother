ab91343ee2787a618fda82f1f8840e57baaad7fa
evx001 <eviexeris@yahoo.com>
2014-03-14 19:36:15 -0400

    associated types

---
+++ yesod/notes001/assocType.hs
@@ -0,0 +1,45 @@
+{-# Language TypeFamilies, OverloadedStrings #-}
+module AssocType where 
+import Data.Word (Word8) 
+import qualified Data.ByteString as S 
+import Data.ByteString.Char8 () -- get an orphan IsString instance
+-- import Text.Html -- this import didn't help with Content typeclass error. 
+  
+-- The goal here is to polymorphic a function so that it will work on more than just lists. We can do this via a type association. 
+
+-- yesod will use associated types this way; there are more uses available in Haskell in general. 
+
+-- LOADS TO HERE --   
+
+class SafeHead a where -- "Safe" is caps because this is a Type we are working with. 
+  type Content a -- adding in "a" solved -- "Number of parameters must match family declaration; expected 0"
+ 
+  safeHead :: a -> Maybe (Content a) 
+  
+instance SafeHead [a] where 
+  type Content [a] = a 
+  safeHead [] = Nothing 
+  safeHead (x:_) = Just x 
+
+instance SafeHead S.ByteString where 
+  type Content S.ByteString = Word8 
+  safeHead bs 
+      | S.null bs = Nothing 
+      | otherwise = Just $ S.head bs 
+main :: IO () -- leaving out unit () will break it!  
+main = do 
+  print $ safeHead ("" :: String) 
+  print $ safeHead ("hello" :: String) 
+
+  print $ safeHead ("" :: String) 
+  print $ safeHead ("hello" :: String) 
+----------------------------------------------
+---- running in the console we get -----------
+--  
+-- runhaskell assocType.hs
+-- Nothing
+-- Just 'h'
+-- Nothing
+-- Just 'h' 
+----------------------------------------------------
+
