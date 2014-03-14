605f4fdd7cf0c5c754fc587c39cd032de190c0af
evx001 <eviexeris@gmail.com>
2014-03-14 16:37:08 -0400

    alex

--- yesod/notes001/2014-0314yesDataTypes.hs
+++ yesod/notes001/2014-0314yesDataTypes.hs
@@ -18,8 +18,36 @@ data Maybe a = Just a | Nothing -- "a" is a type variable of the anytype variety
 -- cabal install alex 
 --  hjsmin version three is required for  the JavaScript minifier Yesod uses to avoid language-javascript package error messages. 
 
+-- cabal install happy
+--
+-- On Debian-based install all of the required C libraries
+-- sudo apt-get install libedit-dev libbsd-dev libgmp3-dev zlib1g-dev freeglut3-dev
+
 --------------------------------------------------------
 -- %APPDATA%\cabal\bin -- is the windows path 
 -- $HOME/.cabal/bin -- is Mac/Linux 
+-- -----------------------------------------------------
+--  cabal-dev	-- to sandbox 
+--  virthualenv -- to sandbox  
+-------------------------------------------------------
+-- language pragmas -- {-# LANGUAGE MyLanguageExtension #-}
+--  GHC command line arguments --  -XMyLanguageExtension
 --
+-- ?? in cabal file, add an extensions block ?? 
 --
+--  {-# LANGUAGE OverloadedStrings, TypeSynonymInstances, FlexibleInstances #-}
+--
+--  ---------------------------------------------------
+--  $(hamletFile "myfile.hamlet") -- Template Haskell
+--  ------------------------------------------------- 
+--   -ddump-splices
+--   ------------------------------------------------
+--
+--   
+--     {-# LANGUAGE QuasiQuotes #-} 
+
+--      [hamlet|<p>This is quasi-quoted Hamlet.|] 
+
+-- The syntax is set off using square brackets and pipes. The name of the quasi-quoter is 
+-- given between the opening bracket and the first pipe, and the content is given between 
+-- the pipes. 
