e2d98d6aa9db0c47552de1ff427e1c634be09386
evx001 <eviexeris@gmail.com>
2014-03-18 01:03:33 -0400

    config

---
+++ 2014-0317yesodConfig.hs
@@ -0,0 +1,18 @@
+-- lets try adding this to the path
+--  windows button --> Environment Variables 
+-- ;C:\Users\evxasus001\Documents\GitHub\yesod\yesod-bin --- nope
+--
+-- cabal update
+-- cabal install cabal-meta cabal-src
+-- intalled to 
+-- C:\Users\evxasus001\AppData\Roaming\cabal\cabal-meta-0.4.1.2\ghc-7.6.3
+--
+{-
+ -In your application folder, create a sources.txt file with the following contents: 
+ ./
+https://github.com/yesodweb/yesod
+https://github.com/yesodweb/shakespeare
+https://github.com/yesodweb/persistent
+https://github.com/yesodweb/wai
+
+-- C:\Users\evxasus001\AppData\Roaming\cabal\bin
