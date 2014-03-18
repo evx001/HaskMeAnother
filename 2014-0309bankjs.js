7cc3a81a5de995a9eac86eb9e847d2ae207da8b2
evx001 <eviexeris@gmail.com>
2014-03-18 01:02:04 -0400

    mean bank stack

--- 2014-0309bankjs.js
+++ 2014-0309bankjs.js
@@ -1,4 +1,6 @@
- 
+/*
+meanBankStack.js 
+
 e-commerce just works, we are living this miricle. 
 	cuase we now don't have to visit a bank in person to do our banking. 
 
@@ -7,11 +9,20 @@ Browsers are quietly
 	it just doesn't let them get in the way.  
 
 MIME type of the file is text/html and the browser executes the file as JavaScript. 
-	This is good because the browser, dispatches a warning as a log entry. 
+
+This is good because the browser, dispatches a warning as a log entry. 
 
 No problem just execute the file and find out more abour it. 
 
+Bank.js --> nodejs--> specialSauce --> mongoDB 
 
+
+
+
+
+
+
+*/ 
 
 
 how-can-haskell-help?
