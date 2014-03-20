52dd209c31198aee110174f103fc69b7717a9aed
evx001 <eviexeris@gmail.com>
2014-03-20 19:49:59 -0400

    Scraper

--- 2014-0320CrListScrape.hs
+++ 2014-0320CrListScrape.hs
@@ -1,6 +1,14 @@
 module ClrLstScrp where 
-
+-- data Customer = MakeCustomer CustomerId String Int
+-- data DialogResponse = Yes|No|Help|Quit 
+--
 -- scrape Crlist 
 --        data syn    [listRaw,[Char]], 
---				        	  [dist? Int], 
---	 				          [rating, (Char,Int)] 
+--                    [rating, (Char,Int)] 
+--                    [dist?, Int]
+type LstRaw = [Char] 
+type Rting   = (Char,Int) 
+type Loc    = [Char]
+
+-- adt -- typeCon -- dataCon --   
+data Jb = MkJb   
