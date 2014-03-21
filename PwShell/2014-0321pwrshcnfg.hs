65b68840e586bc4c8acccf96404b99bb3620b758
evx001 <eviexeris@gmail.com>
2014-03-21 17:59:05 -0400

    more powershell notes

---
+++ PwShell/2014-0321pwrshcnfg.hs
@@ -0,0 +1,10 @@
+# -- Ruby runs slow on windows 
+# -- pwshell DSL 
+# -- desired config available in win 8.1 if on 8.0 upgrade to 8.1 
+# -- MOF Managed Object Framework notation for Desired State Config -- MOF docs --  
+# --  DSC is the local config manager  
+# --
+# configuration MyTechstravaganza { 
+#     node '172.18.-- a single system or class of systems -- server name or fully qual domain name -- pull server uses a GUI --  
+# 
+# } 
