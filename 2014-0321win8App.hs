a64fb202e4980d20eae7c03ef5d3b08940206cab
evx001 <eviexeris@gmail.com>
2014-03-21 17:59:58 -0400

    Win8.1 apps

---
+++ 2014-0321win8App.hs
@@ -0,0 +1,32 @@
+{-
+on-line tool xap file project
+
+htmlJS
+html5 JS  winJS 
+
+native 
+C#/xaml 
+vb/xaml 
+C++/xaml 
+
+
+xamarin 
+  for cross platform 
+
+
+ALl the features and functions 
+        What
+        How 
+          then organize 
+ 
+          UX 
+          Flow 
+
+Interaction Storyboarding 
+
+Visuals -- mockup 
+
+
+
+
+-}
