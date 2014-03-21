bd903913b8e1d4abd2c368049fb5688f3b9a9940
evx001 <eviexeris@gmail.com>
2014-03-21 18:00:34 -0400

    a day of PWshell

---
+++ PwShell/2014-0321pwrshll.sh
@@ -0,0 +1,15 @@
+Formatting Power-Shell 
+
+Out-Defualt -- this where all output pipes through -- finds matching type name -- hash table has no order in .NET 
+
+Start-FormatEngine 
+	XML format -- no doc on this 
+
+
+Show-LastOutput 
+
+ALT + O 
+
+
+ISE pack 
+	
