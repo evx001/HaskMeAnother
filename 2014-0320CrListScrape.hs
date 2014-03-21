12b87ba574299e97592c51a5cf5974c699e04a5e
evx001 <eviexeris@gmail.com>
2014-03-21 17:54:26 -0400

    Scapes okay

--- 2014-0320CrListScrape.hs
+++ 2014-0320CrListScrape.hs
@@ -1,7 +1,12 @@
 module ClrLstScrp where 
+import qualified Data.Text as T -- efficient 
 -- data Customer = MakeCustomer CustomerId String Int
 -- data DialogResponse = Yes|No|Help|Quit 
---
+--  Algebraic Data Types (ADTs), such as 
+type Model = T.Text 
+type Make  = [Char] 
+type GearCount = Int 
+data Vehicle = Bicycle GearCount | Car Make Model
 -- scrape Crlist 
 --        data syn    [listRaw,[Char]], 
 --                    [rating, (Char,Int)] 
