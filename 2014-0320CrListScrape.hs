f156eeeaaeaaee6d9e6502213bc54e4a3f4d5baf
evx001 <eviexeris@gmail.com>
2014-03-21 11:30:15 -0400

    ADT example

--- 2014-0320CrListScrape.hs
+++ 2014-0320CrListScrape.hs
@@ -1,7 +1,12 @@
 module ClrLstScrp where 
+import Data.Test (Text)  
 -- data Customer = MakeCustomer CustomerId String Int
 -- data DialogResponse = Yes|No|Help|Quit 
---
+--  Algebraic Data Types (ADTs), such as 
+type Model = Text 
+type Make  = [Char] 
+type GearCount = Int 
+data Vehicle = Bicycle GearCount | Car Make Model
 -- scrape Crlist 
 --        data syn    [listRaw,[Char]], 
 --                    [rating, (Char,Int)] 
