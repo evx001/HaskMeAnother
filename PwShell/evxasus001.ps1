a0a2ac67a57113c41362301943e6129b08b81876
evx001 <eviexeris@gmail.com>
2014-03-21 17:56:07 -0400

    powerShell Demo

---
+++ PwShell/evxasus001.ps1
@@ -0,0 +1,24 @@
+﻿# run ISE from the command line. 
+
+ # configuration MyTechstravaganza { 
+   # node '10.60.233.4' { 
+    #windowsfeature IIS { 
+     #   name = 'web-server' 
+      #  IncludeAllSubFeature = $true
+       # }
+        #}  
+    
+    # a single system or class of systems -- server name or fully qual domain name -- pull server uses a GUI --  
+New-NetIPAddress -InterfaceAlias homostead -IPAddress 192.168.0.121
+
+# -- Ruby runs slow on windows 
+# -- pwshell DSL 
+# -- desired config available in win 8.1 if on 8.0 upgrade to 8.1 
+# -- MOF Managed Object Framework notation for Desired State Config -- MOF docs --  
+# --  DSC is the local config manager  
+# --
+# configuration MyTechstravaganza { 
+#     node '172.18.-- a single system or class of systems -- server name or fully qual domain name -- pull server uses a GUI --  
+# 
+# } 
+
