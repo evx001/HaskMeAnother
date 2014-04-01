9c444c8653902cc74e291415a4af84e277b99ca4
evx001 <eviexeris@yahoo.com>
2014-04-01 04:29:59 -0400

    Giga1untu Bash script

---
+++ BASH001.bash
@@ -0,0 +1,10 @@
+
+    apt-get install ghc && 
+# Install Ubuntu 12.04 depencies:
+     apt-get install make &&
+     aptitude install -y libgmp3c2 &&
+     apt-get install libncurses5-dev  &&
+
+# some usefuls 
+	apt-get install Htop  &&
+	apt-get install mc  
