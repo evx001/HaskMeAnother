8153cec6816d52381801cf04c72909676de4b020
evx001 <eviexeris@yahoo.com>
2014-04-01 04:53:06 -0400

    git clone

--- BASH__1.bash
+++ BASH__1.bash
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-# BASH001 # installs 
-    apt-get install ghc && apt-get install make && aptitude install -y libgmp3c2 && apt-get install libncurses5-dev && apt-get install Htop && apt-get install mc && apt-get install git  
+# BASH001 # installs to get this file istall git and: 
+# git clone https://github.com/evx001/HaskMeAnother 
+    apt-get install ghc && apt-get install make && aptitude install -y libgmp3c2 && apt-get install libncurses5-dev && apt-get install Htop && apt-get install mc  
 =======
 # BASH001.bash 
 
