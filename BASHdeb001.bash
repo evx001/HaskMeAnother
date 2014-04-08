c94bbf571a163759cd490b59048ab65206e2e058
evx001 <eviexeris@yahoo.com>
2014-04-08 19:56:03 -0400

    getting mc and htop

--- BASHdeb001.bash
+++ BASHdeb001.bash
@@ -1,1 +1,2 @@
-apt-get install build-essential && apt-get install libgmp3-dev libffi-dev zlib1g-dev && apt-get install libgl1-mesa-dev libglu1-mesa-dev && apt-get install libglew-dev freeglut3-dev
+
+apt-get install mc && apt-get install htop && apt-get install build-essential && apt-get install libgmp3-dev libffi-dev zlib1g-dev && apt-get install libgl1-mesa-dev libglu1-mesa-dev && apt-get install libglew-dev freeglut3-dev
