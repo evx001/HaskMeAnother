30cf9c644a6f371347ffcf220a44075866726e08
evx001 <eviexeris@yahoo.com>
2014-04-08 14:45:08 -0400

    swapDeath spot

---
+++ 2014-0408swapDeath.hs
@@ -0,0 +1,22 @@
+-- 2014-0408swapDeath.hs
+
+# ##  @@@@@@@@@@@@@@@@@ Cabal 18 @@@@@@@@@@@@@@@@
+#
+
+#
+# cabal update && 
+cabal install cabal-install-1.18.0.3  
+
+
+[32 of 72] Compiling Distribution.PackageDescription.Parse ( Distribution/PackageDescription/Parse.hs, dist/build/Distribution/PackageDescription/Parse.o )
+Failed to install Cabal-1.18.1.3
+cabal: Error: some packages failed to install:
+Cabal-1.18.1.3 failed during the building phase. The exception was:
+ExitFailure 9
+cabal-install-1.18.0.3 depends on Cabal-1.18.1.3 which failed to install.
+
+# BASH004.bash 
+
+
+
+ 
