9b4f7fb26df86ded29a354b282b30acc70c7d229
evx001 <eviexeris@yahoo.com>
2014-04-08 20:19:58 -0400

    step two echo

--- BASHdeb001.bash
+++ BASHdeb001.bash
@@ -1,6 +1,14 @@
-apt-get update && 
+# apt-get update && 
 # apt-get install mc && apt-get install htop  
- apt-get install build-essential 
+# apt-get install build-essential 
 # apt-get install libgmp3-dev libffi-dev zlib1g-dev 
 # apt-get install libgl1-mesa-dev libglu1-mesa-dev 
 # apt-get install libglew-dev freeglut3-dev
+# -----------------------------------------
+  echo "deb http://ftp.debian.org/debian/ squeeze main" \
+  | sudo tee -a /etc/apt/sources.list
+#      apt-get update
+#      apt-get -t squeeze install libgmp3c2
+#      sed -i '$d' /etc/apt/sources.list
+#      apt-get update
+
