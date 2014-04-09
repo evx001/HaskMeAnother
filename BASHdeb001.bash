cc552ff80e85eb342fdb3456bc68b06a518619fb
evx001 <eviexeris@yahoo.com>
2014-04-08 20:09:09 -0400

    update first before installs

--- BASHdeb001.bash
+++ BASHdeb001.bash
@@ -1,6 +1,6 @@
-
+apt-get update && 
 # apt-get install mc && apt-get install htop  
-# apt-get install build-essential 
+ apt-get install build-essential 
 # apt-get install libgmp3-dev libffi-dev zlib1g-dev 
 # apt-get install libgl1-mesa-dev libglu1-mesa-dev 
 # apt-get install libglew-dev freeglut3-dev
