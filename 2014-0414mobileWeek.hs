cceca0b7149516050a08d3f3296ca6d4aad1843d
evx001 <eviexeris@gmail.com>
2014-04-16 15:20:17 -0400

    MobileWeekMonday

---
+++ 2014-0414mobileWeek.hs
@@ -0,0 +1,224 @@
+ 172.16.1.178 
+
+monitizing apps
+
+	ad placement 
+
+facebook auth -> ad 
+
+43% paid download
+19.1 bil ad rev 
+
+internet ads overtake Television 
+
+mobile e-commerce 
+	$1 - $5 for mobile-commerce  
+	5 to ten years -- internet of things 
+	1 - 2 years to big ticket 
+		apps are already educating people 
+		3 times traditional growth 
+		true mobile-Commerse 
+
+
+Html5 and JS best for pages you don't got to often 
+	
+
+Single API call per screen 
+
+4x your app self built. 
+	4 months projected --> a year 
+
+Optometrist model of app 
+
+security now needs to be in the app not an add-on, automated authentication  
+
+
+HTML offline 
+	app cash 
+		web content off line 
+ 		
+.htaccess file in root 
+	addType text/cache-manafest 
+
+	myfile.manifest
+		cache-ing severside file offline 
+		cache / network / fallback and settings
+			any mistakes and no cache 
+			network 	
+			fallback
+				if no content then go here 
+			settings allows one primary 
+				use what is fresh first 
+		<html manifest="-------.manifest" 
+		cache update 
+			a 404 error means broken cache 
+			5MB storage 
+				chrome allows 20% request of 1/2 disk-space 
+				  
+
+mobile team spotify
+
+bloomberg 
+	
+
+mobileTax 
+	overhead increase by carrier api 
+	6 api average apps 
+	watch for increasing backend network traffic 
+	3rd party api can choke an otherwise performitive app. 
+	power consumption is always going to be important even against responsiveness  
+	Appurify for app testing 
+	Crashes or Hangs are number one complaint 
+		then performance responsiveness 
+		then content / feature issues  
+		2 crashes out of 5 tries kills an app for a user
+	Iphone 4s is still 50% of usage 
+	check for major feature differences when new upgrades in OS 
+		OS memory requirements in 6 to 7 was severe 
+		it is more expensive to get new users than to keep existing users 
+		Know your user base and prioritize so that bug fixes get prioritized to hit the most users 
+	testing frameworks 
+		small QA
+			don't do automation testing 
+				you don't have time
+				you will take valuable time away form other things  
+			borrow a few devices and manually check core user paths 
+			use random app crawlers 
+				
+			but if you must test then appurify  	
+			Apr
+			calabash 
+			  
+civil App
+	Big Apps 
+	mobile Commins 
+	mobile ux @ meetup
+	
+Healthcare .gov was the first major app that had direct political consequences 	
+	
+SMS community 
+	a great way to reach people 
+		spotty internet is prevalent  
+		90% of phone users can at least text 
+		city agencies can use texting to reach clients with more immediacy 
+		
+meetup mobile 	only 14% of userbase 
+		primarily 40%
+		
+google maps  	80% of small biz don't have a web presence 
+			but google maps can help us discover them 
+
+MTA Bus Geoloclized 
+	data is available 
+	challenge .gov
+	more data at opendata people 
+
+NSA 
+	can track us and sniff us anywhere should we be scared 
+	
+net-neutrality 
+	are we head to a Ma-Bell like internet 
+
+Scalability of prize winners 
+
+
+
+ad placement 
+	size and placement 
+	less accidental click 
+	minimal ux disruption  
+	rich media and video 2 - 3 times more effective
+	first party and third party data big monitize potentual
+	
+holiday and seasonal buys
+	
+
+mMedia 
+
+
+Game Development 
+	
+to Gaming from screen writing, 
+	
+
+corona SDK -- for cross platform 
+	it worked 
+	there is a community 
+	good api 
+	I could 
+	windows 8 support 
+	2D design 
+	Unity 3D only 
+		compared to Coco 
+		deploy to xcode and build 
+		or download
+	 
+	one platform first 
+		getting it right 
+
+	Matt Obet 
+		Unity -- designed 
+			many 3D assets 
+		Unity is improving 2D 
+			buggy 
+		mobile --> to Xbox
+		
+	web vs console vs phone 
+		usually single player
+		touch screens are in someways 
+			twitch level 
+		no buttons 
+			touches and swipes only 
+	
+	phones are short attention span devices 
+		really entertaining for a short time 
+		but engaging over the long term. 
+	
+	compulsion loop 
+		the shorter the better 
+	
+	making a game that you would play 
+		
+	Studio-Marcado 
+	
+	fear of early release 
+		get over it 
+	
+	knowing where to go to playtest 
+
+	Regional release 
+
+	monitization 
+		we've tried them all 
+		paying is going through a phase 
+			99cents is dead 
+		
+		1.99
+		
+		(premium $4) or free 
+		
+		payWall 
+			micro-transactions 
+
+		first wave free? 
+			chasing the payWall is questionable
+		
+		when indie == commercial failure 
+			I make games for fun, I freelance to pay the bills 		
+	
+	pussy giggles solve-it yourself mystery 
+
+	tablet & mobile and education for rich learning 
+	
+	voice and gesture 
+		glass 
+	
+	not micro managing 
+		kludgey, touching is precise. 
+		we want larger gestures 
+		eleet motion 
+	
+	April 22nd playtest 
+
+
+	NYC mobile forum 
