d499c9b605338538ecd44c2b7909baa39db15d46
evx001 <eviexeris@gmail.com>
2014-04-15 23:42:18 -0400

    Bash on CemtOS

---
+++ YUMBASH001.bash
@@ -0,0 +1,24 @@
+# yum install curl-devel expat-devel gettext-devel \ openssl-devel zlib-devel
+# 
+# 
+# wget  http://git-scm.com/download
+# 
+  tar -zxf git-1.7.2.2.tar.gz
+# 
+# cd git-1.7.2.2
+# 
+# make prefix=/usr/local all
+# 
+# make prefix=/usr/local install
+# 
+# 
+# sudo make prefix=/usr/local install
+# 
+# yum install git 
+#  
+# 
+# sudo yum update
+# sudo yum search haskell
+# sudo yum search ghc
+# 
+# sudo yum install -y haskell-platform
