3fb9d5c713c311a167416b1c7bb9d5d2c564a48d
evx001 <eviexeris@gmail.com>
2014-04-26 08:21:26 -0400

    chap2 mena

---
+++ chapter2/section2/example.hs
@@ -0,0 +1,22 @@
+module Chapter2.section2.example where
+
+-- this would example.hs  
+--
+import Data.List 
+
+-- modules are either exposed, 
+exposed-modules: 
+-- or 
+other-modules: 
+
+-- @ the command line you can run 
+cabal configure
+-- followed by 
+cabal build 
+-- try to place stuff in
+src -- folder 
+New Haskell Module 
+
+library 
+
+
