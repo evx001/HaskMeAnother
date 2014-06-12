3b85455ea553c8d9558fa2152456945875df5ccf
evx001 <eviexeris@gmail.com>
2014-06-12 18:02:26 -0400

    going against C

---
+++ yyz.hs
@@ -0,0 +1,27 @@
+module C_x_comp where 
+
+import Prelude qualified as P  
+
+-- dvd :: Int -> Int    
+-- dvd n = 20 + n 
+-------------------------------------------
+-- #include <stdio.h>
+-- 
+-- int main()
+-- {
+-- 	int many[] = {2,4,8} ;
+--     	int sum = 0;
+--     	for (int i=0; i<3; i++) {
+--         	sum += many[i];
+--         	printf("sum is %d\n",sum);
+--     }
+-- 	return 0;
+-- }
+
+-- fn001 = sum [2, 4, 8]
+------------------------------
+-- sUM = \_ -> [2,4,8]  
+
+
+
+------------------------------------------
